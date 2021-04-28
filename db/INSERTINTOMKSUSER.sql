bd8cdd75dea085baff584821931573fe64df0245
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-04-28 19:08:42 +0700

    fix mismatched data type

--- db/INSERTINTOMKSUSER.sql
+++ db/INSERTINTOMKSUSER.sql
@@ -1,9 +1,9 @@
-INSERT INTO MksUser VALUES ('', 'thai.tran@fulbright.edu.vn', 'Thai', 'Tran', 'Thanh', '090-336-9880', 'Maker Space', 'Associate Director of Design & Fabrication Operations','','');
-INSERT INTO MksUser VALUES ('190028', 'hoc.nguyen.190028@student.fulbright.edu.vn', 'Hoc', 'Nguyen', 'Ba', '0961822808', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190005', 'anh.nguyen.190005@student.fulbright.edu.vn', 'Anh', 'Nguyen', 'Hoang Nam', '033-893-7594', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190042', 'lan.pham.190042@student.fulbright.edu.vn', 'Lan', 'Pham', 'Hoang', '0867899303', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190034', 'khanh.phan.190034@student.fulbright.edu.vn', 'Khanh', 'Phan', 'Ngoc Lan', '0788566911', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190048', 'minh.phan.190048@student.fulbright.edu.vn', 'Minh', 'Phan', 'Nguyen Tuong', '0819973512', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190011', 'anh.trinh.190011@student.fulbright.edu.vn', 'Anh', 'Trinh', 'Ha', '0979080154', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('190057', 'nhi.vu.190057@student.fulbright.edu.vn', 'Nhi', 'Vu', 'Thuan', '0898140601', 'UG19', 'Student','','');
-INSERT INTO MksUser VALUES ('200087', 'sieu.vuong.200087@student.fulbright.edu.vn', 'Sieu', 'Vuong', 'Kha', '0949295493', 'UG20', 'Student','','');
+INSERT INTO MksUser VALUES ('', 'thai.tran@fulbright.edu.vn', 'Thai', 'Tran', 'Thanh', '090-336-9880', 'Maker Space', 'Associate Director of Design & Fabrication Operations','');
+INSERT INTO MksUser VALUES ('190028', 'hoc.nguyen.190028@student.fulbright.edu.vn', 'Hoc', 'Nguyen', 'Ba', '0961822808', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190005', 'anh.nguyen.190005@student.fulbright.edu.vn', 'Anh', 'Nguyen', 'Hoang Nam', '033-893-7594', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190042', 'lan.pham.190042@student.fulbright.edu.vn', 'Lan', 'Pham', 'Hoang', '0867899303', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190034', 'khanh.phan.190034@student.fulbright.edu.vn', 'Khanh', 'Phan', 'Ngoc Lan', '0788566911', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190048', 'minh.phan.190048@student.fulbright.edu.vn', 'Minh', 'Phan', 'Nguyen Tuong', '0819973512', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190011', 'anh.trinh.190011@student.fulbright.edu.vn', 'Anh', 'Trinh', 'Ha', '0979080154', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('190057', 'nhi.vu.190057@student.fulbright.edu.vn', 'Nhi', 'Vu', 'Thuan', '0898140601', 'UG19', 'Student','');
+INSERT INTO MksUser VALUES ('200087', 'sieu.vuong.200087@student.fulbright.edu.vn', 'Sieu', 'Vuong', 'Kha', '0949295493', 'UG20', 'Student','');
