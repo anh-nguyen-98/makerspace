ddb496d5ae5eb5c6aabfdcd47ba2b8af3502ba27
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-05-24 21:21:52 +0700

    add datasource category bar

--- Makerspace/Makerspace/Equipment.aspx.designer.cs
+++ Makerspace/Makerspace/Equipment.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Makerspace
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView categoryList;
+
+        /// <summary>
+        /// MakerspaceDB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource MakerspaceDB;
     }
 }
