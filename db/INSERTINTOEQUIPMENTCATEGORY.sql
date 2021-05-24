51de3dce3991c3ffc99f4e88d2e65728136e4ab1
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-05-24 18:58:23 +0700

    insert values to equip. category

---
+++ db/INSERTINTOEQUIPMENTCATEGORY.sql
@@ -0,0 +1,5 @@
+INSERT INTO EquipmentCategory (catName) VALUES ('Electronics');
+INSERT INTO EquipmentCategory (catName) VALUES ('Mechanics');
+INSERT INTO EquipmentCategory (catName) VALUES ('Printing');
+INSERT INTO EquipmentCategory (catName) VALUES ('Small Tools');
+INSERT INTO EquipmentCategory (catName) VALUES ('Small Tools');
