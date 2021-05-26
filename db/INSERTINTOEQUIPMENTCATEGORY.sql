15241731a117307e9d92311b994bfa3030dc5b92
tuongminhphan <tuongminh.23301@gmail.com>
2021-05-26 23:42:13 +0700

    Database fix

--- db/INSERTINTOEQUIPMENTCATEGORY.sql
+++ db/INSERTINTOEQUIPMENTCATEGORY.sql
@@ -2,4 +2,4 @@ INSERT INTO EquipmentCategory (catName) VALUES ('Electronics');
 INSERT INTO EquipmentCategory (catName) VALUES ('Mechanics');
 INSERT INTO EquipmentCategory (catName) VALUES ('Printing');
 INSERT INTO EquipmentCategory (catName) VALUES ('Small Tools');
-INSERT INTO EquipmentCategory (catName) VALUES ('Small Tools');
+INSERT INTO EquipmentCategory (catName) VALUES ('Large Tools');
