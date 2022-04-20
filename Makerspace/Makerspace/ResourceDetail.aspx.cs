ab56f088a54fd6cf3a01d951a28a25c92db55a69
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2022-04-20 18:30:11 +0700

    add listview for subitems

---
+++ Makerspace/Makerspace/ResourceDetail.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace Makerspace
+{
+    public partial class ResourceDetail : System.Web.UI.Page
+    {
+        protected static int ItemsCount = 0;
+        protected static string CONSTRING = ConfigurationManager.ConnectionStrings["MakerspaceDBConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                load();
+            }
+            else
+            {
+
+            }
+
+        }
+
+        //Post back data
+        //Bind ListView
+        protected void load()
+        {
+            using (SqlConnection con = new SqlConnection(CONSTRING))
+            {
+                SqlCommand cmd = new SqlCommand("uspReadAllEquipment");
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.Connection = con;
+                DataTable db = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(db);
+                ResourceDetail_Listview.DataSource = db;
+                ResourceDetail_Listview.DataBind();
+            }
+        }
+
+    }
+
+}
