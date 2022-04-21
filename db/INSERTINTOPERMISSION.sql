de0959f1ec21a198c906d3b907be2f377d01b38c
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2022-04-21 19:15:16 +0700

    remove redundant sql script

--- db/INSERTINTOPERMISSION.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Permission VALUES('Read');
-INSERT INTO Permission VALUES('Write');
--- db/INSERTINTOROOM.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO	 Room	 VALUES	 (	'M', 	'Maker Room') ;
-INSERT INTO	 Room	 VALUES	 (	'S', 	'Synergy Room') ;
