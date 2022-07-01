9bdf1082c78e6f95bd82a4722175532bd66d2843
minhtnphan <tuongminh.23301@gmail.com>
2022-07-01 18:28:05 +0700

    remove unnecessary dependencies

--- Makerspace/Makerspace/ResourceDetail.aspx.designer.cs
+++ Makerspace/Makerspace/ResourceDetail.aspx.designer.cs
@@ -60,15 +60,6 @@ namespace Makerspace
         protected global::System.Web.UI.WebControls.Label name;
 
         /// <summary>
-        /// editBtn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button editBtn;
-
-        /// <summary>
         /// count_equip control.
         /// </summary>
         /// <remarks>
