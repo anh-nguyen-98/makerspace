4628f93038b09a125ce76b2bdbf15573800a19ac
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2022-09-11 16:11:55 +0700

    popup format

--- Makerspace/Makerspace/Site.Master.cs
+++ Makerspace/Makerspace/Site.Master.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Web;
@@ -15,7 +18,7 @@ namespace Makerspace
         private const string AntiXsrfTokenKey = "__AntiXsrfToken";
         private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
         private string _antiXsrfTokenValue;
-
+        protected static string CONSTRING = ConfigurationManager.ConnectionStrings["MakerspaceDBConnectionString"].ConnectionString;
         protected void Page_Init(object sender, EventArgs e)
         {
             // The code below helps to protect against XSRF attacks
@@ -82,6 +85,81 @@ namespace Makerspace
             Session.RemoveAll();
             Response.Redirect("~/Resource.aspx");
 
+        }
+        protected void LogIn(object sender, EventArgs e)
+        {
+            EmailLabel.CssClass = "input-label";
+            Email.CssClass = "input-box";
+
+            EmailErrorMessage.Text = "";
+            PasswordErrorMessage.Text = "";
+
+            if (hasEmptyFields()) return;
+            if (!validateEmail()) return;
+
+            using (SqlConnection con = new SqlConnection(CONSTRING))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Account where email = @email and password = @password", con);
+                cmd.Parameters.AddWithValue("@email", Email.Text);
+                cmd.Parameters.AddWithValue("@password", Password.Text);
+                con.Open();
+                DataTable db = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(db);
+                if (db.Rows.Count > 0)
+                {
+                    Session.RemoveAll();
+                    Session["role"] = db.Rows[0]["role"];
+                    Response.Redirect("~/Resource.aspx");
+                }
+                else
+                {
+                    PasswordLabel.CssClass = "input-label-danger";
+                    Password.CssClass = "input-box-danger";
+                    PasswordErrorMessage.Text = "Your password is incorrect.";
+                }
+            }
+
+        }
+        protected bool validateEmail()
+        {
+            using (SqlConnection con = new SqlConnection(CONSTRING))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Account where email = @email", con);
+                cmd.Parameters.AddWithValue("@email", Email.Text);
+                con.Open();
+                DataTable db = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(db);
+                if (db.Rows.Count == 0)
+                {
+                    EmailLabel.CssClass = "input-label-danger";
+                    Email.CssClass = "input-box-danger";
+                    EmailErrorMessage.Text = "This email is invalid.";
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        protected bool hasEmptyFields()
+        {
+            bool flag = false;
+            if (Email.Text.Length == 0)
+            {
+                EmailLabel.CssClass = "input-label-danger";
+                Email.CssClass = "input-box-danger";
+                EmailErrorMessage.Text = "Email cannot be empty.";
+                flag = true;
+            }
+            if (Password.Text.Length == 0)
+            {
+                PasswordLabel.CssClass = "input-label-danger";
+                Password.CssClass = "input-box-danger";
+                PasswordErrorMessage.Text = "Password cannot be empty.";
+                flag = true;
+            }
+            return flag;
         }
 
     }
