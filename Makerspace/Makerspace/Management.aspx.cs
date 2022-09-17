46e574b86858122ff14af45196d9b96549e9e39b
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2022-09-17 23:39:10 +0700

    update permission edit

--- Makerspace/Makerspace/Management.aspx.cs
+++ Makerspace/Makerspace/Management.aspx.cs
@@ -11,25 +11,10 @@ namespace Makerspace
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(Session["level"]);
-            if (IsPostBack) {
-                //if (Session["level"] == null || Convert.ToInt16(Session["level"].ToString()) >= 1)
-                //{
-                //    GridView1.Columns[0].Visible = false;
-                //}
-                if (Session["level"] != null && Convert.ToInt16(Session["level"].ToString()) <= 1)
-                {
-                    GridView1.Columns[0].Visible = true;
-                
-                }
-                else
-                {
-                    GridView1.Columns[0].Visible = false;
-                   
-                }
+            if (Session["level"] == null || Convert.ToInt16(Session["level"].ToString()) > 1)
+            {
+                GridView1.Columns[0].Visible = false;
             }
-           
-            
         }
     }
 }
